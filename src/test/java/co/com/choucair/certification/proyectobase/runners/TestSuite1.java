f304e4612ff24bf1bb1a02304426a3e07e89c385
WilliamMaldonados <wmaldona@bancolombia.com.co>
2024-04-16 15:11:05 -0500

    update test 3

---
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/TestSuite1.java
@@ -0,0 +1,11 @@
+package co.com.choucair.certification.proyectobase.runners;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Test1.class, Test2.class, Test3.class, Test4.class, Test5.class, TestFM.class})
+
+
+public class TestSuite1 {
+}
