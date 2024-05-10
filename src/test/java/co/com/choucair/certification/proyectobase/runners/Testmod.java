6c7b21225971fa6f14933a1612d48799115fd276
WilliamMaldonados <wmaldona@bancolombia.com.co>
2024-05-10 15:29:17 -0500

    update runnner

--- src/test/java/co/com/choucair/certification/proyectobase/runners/Testmod.java
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/Testmod.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({Test1Modify.class, Test2Modify.class, Test3Modify.class, Test4Modify.class, Test5Modify.class, Test3Modify.class})
+@Suite.SuiteClasses({Test1Modify.class, Test2Modify.class, Test3Modify.class, Test4Modify.class, Test5Modify.class, Test3aModify.class})
 public class Testmod {
 }
