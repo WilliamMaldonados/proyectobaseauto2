7a14b59be52d2637c6decbbda9145e3981dd5c36
WilliamMaldonados <wmaldona@bancolombia.com.co>
2024-05-10 12:07:00 -0500

    update devolutions test and runner for migration

--- src/test/java/co/com/choucair/certification/proyectobase/runners/TestDev.java
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/TestDev.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({Test1Devolution.class, Test2Devolution.class, Test3a.class, Test4Devolution.class, Test5Devolution.class})
+@Suite.SuiteClasses({Test1Devolution.class, Test2Devolution.class,Test3Devolution.class,Test4Devolution.class, Test5Devolution.class})
 public class TestDev {
 }
